849254abd98d8815d549f5f5e41b0683508dfd12
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-26 16:27:31 +0530

    tools section colmplete

--- src/App.jsx
+++ src/App.jsx
@@ -223,6 +223,17 @@ function App() {
                 <BriefcaseBusiness size={20} className="text-green-400"/>
                 <span>Favorite Tools</span>
               </h2>
+
+              <ul className="space-y-2 text-sm">
+                {tools.map((tool, i) => (
+                  <motion.li key={i}
+                  whileHover={{x:4}}
+                  className="flex items-center gap-2 group">
+                    <span className="bullet bg-green-400/50 group-hover:bg-green-400"></span>
+                    {tool}
+                  </motion.li>
+                ))}
+              </ul>
           </motion.div>
         </motion.main>
       </div>
